6adac39f20e713b1804645702779f9d3f4d1458e
mew7wo <mew7wo@gmail.com>
2013-01-31 09:37:51 +0800

    fetch

--- utils/fetch.py
+++ utils/fetch.py
@@ -11,31 +11,32 @@ import cookielib
 import socket
 
 
-class Fetch(object):
+class Fetcher(object):
     ''' url fetch class '''
     def __init__(self):
         self._headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) Chrome/23.0.1271.64 Safari/537.11'}
         self.__login()
 
     def __login(self):
-        http_handler = urllib2.HTTPHandler(debuglevel=1)
         cj = cookielib.CookieJar()
+        http_handler = urllib2.HTTPHandler(debuglevel=1)
         self._opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj), http_handler)
-
         post_data = urllib.urlencode({'form_email':'1398882026@qq.com', 'form_password':'liumengchao'})
         login_url = 'http://www.douban.com/accounts/login'
         login_req = urllib2.Request(login_url, data=post_data, headers=self._headers)
         login_resp = self.__get(login_req)
-        if login_resp.getcode() != '200':
-            raise LoginError()
+
+        if login_resp.getcode() != 200:
+            raise LoginError(login_resp.getcode())
             
     def __get(self, req):
-        content = None
+        resp = None
         try:
-            content = self._opener.open(req) 
+            resp = self._opener.open(req) 
         except (urllib2.URLError, socket.timeout), e:
             print repr(e)
-        return content
+        finally:
+            return resp 
 
     def get(self, url):
         req = urllib2.Request(url, headers=self._headers)
@@ -43,7 +44,7 @@ class Fetch(object):
         return content
         
 
-class LoginError(object):
+class LoginError(Exception):
     def __init__(self, msg=None):
         self.msg = msg
 
