40b62d297e572b9e896113e30858974abaacd288
mew7wo <mew7wo@gmail.com>
2013-01-31 16:33:52 +0800

    dfasf

--- utils/fetch.py
+++ utils/fetch.py
@@ -13,17 +13,20 @@ import socket
 
 class Fetcher(object):
     ''' url fetch class '''
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) Chrome/23.0.1271.64 Safari/537.11'}
+    opener = None
     def __init__(self):
-        self._headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) Chrome/23.0.1271.64 Safari/537.11'}
+        if self.__class__.opener == None:
+            cj = cookielib.CookieJar()
+            http_handler = urllib2.HTTPHandler(debuglevel=1)
+            self.__class__.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj), http_handler)
+
         self.__login()
 
     def __login(self):
-        cj = cookielib.CookieJar()
-        http_handler = urllib2.HTTPHandler(debuglevel=1)
-        self._opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj), http_handler)
         post_data = urllib.urlencode({'form_email':'1398882026@qq.com', 'form_password':'liumengchao'})
         login_url = 'http://www.douban.com/accounts/login'
-        login_req = urllib2.Request(login_url, data=post_data, headers=self._headers)
+        login_req = urllib2.Request(login_url, data=post_data, headers=self.__class__.headers)
         login_resp = self.__get(login_req)
 
         if login_resp.getcode() != 200:
@@ -32,14 +35,14 @@ class Fetcher(object):
     def __get(self, req):
         resp = None
         try:
-            resp = self._opener.open(req) 
+            resp = self.__class__.opener.open(req) 
         except (urllib2.URLError, socket.timeout), e:
             print repr(e)
         finally:
             return resp 
 
     def get(self, url):
-        req = urllib2.Request(url, headers=self._headers)
+        req = urllib2.Request(url, headers=self.__class__.headers)
         content = self.__get(req).read()
         return content
         
